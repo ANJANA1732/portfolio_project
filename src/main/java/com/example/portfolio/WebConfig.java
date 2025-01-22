25aec49af1e35aaa24d145e985d295df9ea349a9
ANJANA1732 <anjusivakumar17302@gmail.com>
2025-01-23 00:19:37 +0530

    backend updation through branch

--- src/main/java/com/example/portfolio/WebConfig.java
+++ src/main/java/com/example/portfolio/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")  // This maps to the `/api` endpoints in your backend
-                .allowedOrigins("http://localhost:3002")  // The URL of your frontend
+                .allowedOrigins("http://localhost:3004")  // The URL of your frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allowed HTTP methods
                 .allowedHeaders("*");  // Allow all headers
     }
